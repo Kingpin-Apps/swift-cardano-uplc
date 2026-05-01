53221983501a87fdf79765f64029b93700c9f4a6
Hareem Adderley <hadderley@kingpinapps.com>
2026-05-01 06:48:23 -0500

    chore: update swift dependencies to latest versions
    Update swift-cardano-core from 0.2.31 to 0.3.16, swift-log from
    1.11.0 to 1.12.0, and swift-mnemonic from 0.2.1 to 0.2.2 to get
    latest features and bug fixes

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
         .library(name: "SwiftCardanoUPLC", targets: ["SwiftCardanoUPLC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.2.26"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.3.16"),
         .package(url: "https://github.com/Kingpin-Apps/swift-blst.git", from: "0.1.2"),
         .package(url: "https://github.com/Kingpin-Apps/swift-ncal.git", from: "0.2.2"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.9.0"),
         .package(url: "https://github.com/21-DOT-DEV/swift-secp256k1", exact: "0.22.0"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.12.0"),
     ],
     targets: [
         .target(
